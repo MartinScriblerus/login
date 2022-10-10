5b6841e4de8d60de9f00f0761d8d7e70b19e8954
Matthew Reilly <matthewfreilly@gmail.com>
2022-10-10 03:38:14 -0500

    auth test

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,8 +1,17 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import { useSession } from "next-auth/react";
+
+import LogInOutButton from '../components/login-btn'
 
 export default function Home() {
+
+  const { data: session } = useSession()
+  if(session){
+      console.log('in index', session);
+  } 
+
   return (
     <div className={styles.container}>
       <Head>
@@ -15,7 +24,7 @@ export default function Home() {
         <h1 className={styles.title}>
           Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
-
+        <LogInOutButton />
         <p className={styles.description}>
           Get started by editing{' '}
           <code className={styles.code}>pages/index.js</code>
