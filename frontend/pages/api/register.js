f3a046278d4d2265adb2510bc73de60c21e485aa
Matthew Reilly <matthewfreilly@gmail.com>
2022-10-16 18:05:09 -0500

    pass hash in obj

--- frontend/pages/api/register.js
+++ frontend/pages/api/register.js
@@ -31,26 +31,29 @@ export default async function postCreateUser (req, res) {
   }
   console.log("input in register is: ", input)
 
-  async function tryHashPass() {
-    return await bcrypt.hash(input.email, saltRounds, function(err, hash) {
+  var createdUser; 
+
+    let hashPassObj = {
+      pass: 'test'
+    }
+    bcrypt.hash(input.email, saltRounds, function(err, hash) {
       // Store hash in your password DB.
       // return hash
     console.log("HASH PASS: ", hash);
-    return hash
+    // return hash
+      hashPassObj.pass = hash;
+      return hashPassObj;
     // let data = Object.values(input).map(i=>i)[0];
 
     })
-  }
 
-  const hashPass = (async () => {
-    return await tryHashPass()
-  })()
+  } 
 
   var createdUser = await prisma.users.create({
     data: {
       user_name: req.body.user_name,
       // email: req.body.email,
-      email: JSON.stringify(hashPass)
+      email: hashPassObj.pass
       // email_verified: null,
       // image: null,
       // // created_at: DateTime,
