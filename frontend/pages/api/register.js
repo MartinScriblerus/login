6a40d67eacc7780ebf43992ee6dc96a29332bf5f
Matthew Reilly <matthewfreilly@gmail.com>
2022-10-16 18:31:17 -0500

    restructure async

--- frontend/pages/api/register.js
+++ frontend/pages/api/register.js
@@ -32,25 +32,28 @@ export default async function postCreateUser (req, res) {
   console.log("input in register is: ", input)
 
 
-
-  // try {
-  let hashPass = bcrypt.hash(input.email, saltRounds, function(err, hash) {
-    // Store hash in your password DB.
-    // return hash
-    console.log("HASH PASS: ", hash);
-    return hash;
-
-  // let data = Object.values(input).map(i=>i)[0];
-
-  })
+  async function hashPass(email){
+    // try {
+    let hashPass;
+    
+    bcrypt.hash(email, saltRounds, function(err, hash) {
+      // Store hash in your password DB.
+      // return hash
+      hashPass = hash;
+      console.log("HASH PASS: ", hash);
+      return hashPass;
+    // let data = Object.values(input).map(i=>i)[0];
+    })
+    return hashPass;
+  }
   console.log("what is hashpass? ", hashPass);
-
+  const encryptedPass = await hashPass(input.email);
 
   var createdUser = await prisma.users.create({
     data: {
       user_name: req.body.user_name,
       // email: req.body.email,
-      email: hashPass
+      email: encryptedPass
       // email_verified: null,
       // image: null,
       // // created_at: DateTime,
