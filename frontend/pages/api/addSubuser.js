98ab61ec39a95bdef9992e83a4fec00500f51e36
Matthew Reilly <matthewfreilly@gmail.com>
2022-10-17 13:13:18 -0500

    major retraction to addSubusers

--- frontend/pages/api/addSubuser.js
+++ frontend/pages/api/addSubuser.js
@@ -23,49 +23,88 @@ export default async function postAddSubuser (req, res) {
 
 
 
-    ///////////
-    async function getUserByName(user_name){
+    // ///////////
+    // async function getUserByName(user_name){
     
-        return prisma.users.findMany({
-          where: {
-              user_name: {
-              equals:user_name
-              }
-            },
-        })
+    //     return prisma.users.findMany({
+    //       where: {
+    //           user_name: {
+    //           equals:user_name
+    //           }
+    //         },
+    //     })
 
-        // console.log("resultttt ", result);
+    //     // console.log("resultttt ", result);
 
 
-        // return result;
-        }
+    //     // return result;
+    //     }
 
-        let userToUpdate= getUserByName(req.body.user_name).then(response=>{return response});
+    //     let userToUpdate= getUserByName(req.body.user_name).then(response=>{return response});
         
-        console.log("USER TO UPDATE: ", userToUpdate);
+    //     console.log("USER TO UPDATE: ", userToUpdate);
 
-    ///////////
+    // ///////////
+    // if(!userToUpdate){
+    //     console.log("no user to update");
+    //     return null;
+    // }
+    // if(!Object.values(userToUpdate)[0]){
+    //     return;
+    // }
+    // // console.log("this? ", Object.values(userToUpdate)[0].subusers_array);
+    // if(Object.values(userToUpdate)[0].subusers_array.indexOf(req.body.subuserName) === -1){
+    //     console.log("pushing subuser name: ", req.body.subuserName);
+    //     Object.values(userToUpdate)[0].subusers_array.push(req.body.subuserName)  
+    // } 
+
+    //     // res = prisma.users.findMany({
+    //     //     where: { user_name: userToUpdate.user_name },
+    //     //     select: {
+    //     //         data:{subusers_array: true}},
+    //     //     })
+
+    //    // console.log("about to update with: ", Object.values(userToUpdate)[0].subusers_array);
+    
+    // prisma.users.update({
+    //     data: {
+    //         subusers_array: {
+    //             set: Object.values(userToUpdate)[0].subusers_array
+    //         },
+    //     },
+    //     where: { id : Object.values(userToUpdate)[0].id},
+    // }).then(result => {
+    //     console.log("RES in ADD ", result)
+    //     // return result;
+    //     // if(typeof result !== String){
+    //     //     result = result.toString();
+    //     // }
+    //     // res.end(result);
+    //     return result; 
+    // })
+
+    // return;
+    let userToUpdate = await prisma.users.findMany({
+        where: {
+            user_name: {
+                contains: req.body.user_name,
+            }, 
+        }
+    })
     if(!userToUpdate){
         console.log("no user to update");
-        return null;
-    }
-    if(!Object.values(userToUpdate)[0]){
         return;
     }
-    // console.log("this? ", Object.values(userToUpdate)[0].subusers_array);
+
     if(Object.values(userToUpdate)[0].subusers_array.indexOf(req.body.subuserName) === -1){
-        console.log("pushing subuser name: ", req.body.subuserName);
         Object.values(userToUpdate)[0].subusers_array.push(req.body.subuserName)  
     } 
 
-        // res = prisma.users.findMany({
-        //     where: { user_name: userToUpdate.user_name },
-        //     select: {
-        //         data:{subusers_array: true}},
-        //     })
-
-       // console.log("about to update with: ", Object.values(userToUpdate)[0].subusers_array);
-    
+    // res = prisma.users.findMany({
+    //     where: { user_name: userToUpdate.user_name },
+    //     select: {
+    //         data:{subusers_array: true}},
+    //     })
     prisma.users.update({
         data: {
             subusers_array: {
@@ -75,13 +114,7 @@ export default async function postAddSubuser (req, res) {
         where: { id : Object.values(userToUpdate)[0].id},
     }).then(result => {
         console.log("RES in ADD ", result)
-        // return result;
-        // if(typeof result !== String){
-        //     result = result.toString();
-        // }
-        // res.end(result);
-        return result; 
-    })
+        // return res
+    }).catch(()=>{}).finally(()=>{return res}); 
 
-    return;
 } 
