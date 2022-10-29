0e1ea10eded6220ef2cd9c13f3151586dfb537c8
Matthew Reilly <matthewfreilly@gmail.com>
2022-10-29 02:39:59 -0500

    fi config

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,14 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // reactStrictMode: true,
+  reactStrictMode: true,
   swcMinify: true,
-  webpack: function (config, { isServer }) {
-    // Your other webpack configs
-
-    config.experiments = { ...config.experiments, asyncWebAssembly: true }
-
-    return config
-  }
 }
 
 
